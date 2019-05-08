a9403ad3a471943241227220e8ce06fa2192e16c
Fernando Mez <fermezz@gmail.com>
2019-05-08 16:56:46 -0300

    Make function to generate random numbers of specific ciphers.

--- guessthenumber/game.py
+++ guessthenumber/game.py
@@ -1,2 +1,15 @@
+import random
+
+
 def play():
     return True
+
+def think_number(cipher_quantity=4):
+
+    stringified_number = ''.join(
+        map(
+            str, [random.randint(0, 9) for i in range(cipher_quantity)]
+        )
+    )
+
+    return int(stringified_number if stringified_number else '0')
