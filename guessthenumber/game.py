3f6efb7f5166eedf8414b5c007e796dd7015b425
Fernando Mez <fermezz@gmail.com>
2019-05-08 17:30:32 -0300

    Implement take guess functionality.

--- guessthenumber/game.py
+++ guessthenumber/game.py
@@ -5,7 +5,6 @@ def play():
     return True
 
 def think_number(cipher_quantity=4):
-
     stringified_number = ''.join(
         map(
             str, [random.randint(0, 9) for i in range(cipher_quantity)]
@@ -13,3 +12,41 @@ def think_number(cipher_quantity=4):
     )
 
     return int(stringified_number if stringified_number else '0')
+
+
+def take_guess(guess, number):
+    guess = str(guess)
+    number = str(number)
+
+    if guess == number:
+        return Answer(len(number), 0, 0)
+
+    rights = 0
+    wrongs = 0
+    present_but_wrong = 0
+    for idx, char in enumerate(guess):
+        if char == number[idx]:
+            rights += 1
+        elif char in number:
+            present_but_wrong += 1
+        else:
+            wrongs += 1
+
+    return Answer(rights, wrongs, present_but_wrong)
+
+
+
+class Answer:
+
+    def __init__(self, rights, wrongs, present_but_wrong):
+        self.rights = rights
+        self.wrongs = wrongs
+        self.present_but_wrong = present_but_wrong
+
+    def __str__(self):
+        return 'Rights: {rights}. Wrongs: {wrongs}. Present but wrong: {present_but_wrong}.'.format(
+            rights=self.rights,
+            wrongs=self.wrongs,
+            present_but_wrong=self.present_but_wrong,
+        )
+
--- tests/test_game.py
+++ tests/test_game.py
@@ -2,6 +2,7 @@ from mock import patch
 
 from guessthenumber.game import (
     play,
+    take_guess,
     think_number,
 )
 
@@ -9,16 +10,57 @@ from guessthenumber.game import (
 def test_game():
     assert play()
 
-@patch('guessthenumber.game.random.randint', return_value=1)
-def test_think_number_four_ciphers(mocked_randint):
-    assert 1111 == think_number()
 
-@patch('guessthenumber.game.random.randint', return_value=1)
-def test_think_number_zero_ciphers(mocked_randint):
-    """Return always 0 if number is cipher quantity is zero."""
-    assert 0 == think_number(cipher_quantity=0)
+class TestThinkNumber:
 
-@patch('guessthenumber.game.random.randint', return_value=1)
-def test_think_number_minus_one_ciphers(mocked_randint):
-    """Return always 0 if number is negative."""
-    assert 0 == think_number(cipher_quantity=-1)
+    @patch('guessthenumber.game.random.randint', return_value=1)
+    def test_think_number_four_ciphers(self, mocked_randint):
+        assert 1111 == think_number()
+
+    @patch('guessthenumber.game.random.randint', return_value=1)
+    def test_think_number_zero_ciphers(self, mocked_randint):
+        """Return always 0 if number is cipher quantity is zero."""
+        assert 0 == think_number(cipher_quantity=0)
+
+    @patch('guessthenumber.game.random.randint', return_value=1)
+    def test_think_number_minus_one_ciphers(self, mocked_randint):
+        """Return always 0 if number is negative."""
+        assert 0 == think_number(cipher_quantity=-1)
+
+
+class TestTakeGuess:
+
+    def test_take_guess(self):
+        answer = take_guess(1234, 1234)
+
+        assert 4 == answer.rights
+        assert 0 == answer.wrongs
+        assert 0 == answer.present_but_wrong
+
+    def test_take_guess_three_wrongs(self):
+        answer = take_guess(1111, 1234)
+
+        assert 1 == answer.rights
+        assert 0 == answer.wrongs
+        assert 3 == answer.present_but_wrong
+
+    def test_take_guess_two_rights_one_wrong_one_pbw(self):
+        answer = take_guess(1234, 1273)
+
+        assert 2 == answer.rights
+        assert 1 == answer.wrongs
+        assert 1 == answer.present_but_wrong
+
+    def test_take_guess_one_right_three_wrongs(self):
+        answer = take_guess(1234, 8564)
+
+        assert 1 == answer.rights
+        assert 3 == answer.wrongs
+        assert 0 == answer.present_but_wrong
+
+    def test_take_guess_no_rights_four_pbw(self):
+        answer = take_guess(1234, 4321)
+
+        assert 0 == answer.rights
+        assert 0 == answer.wrongs
+        assert 4 == answer.present_but_wrong
